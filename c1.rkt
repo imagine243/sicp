a831384ef6e8899b8686b816bce962d8cd2bf3aa
imagine243 <imagine243@gmail.com>
2017-09-27 18:40:32 +0800

    add new function

---
+++ c1.rkt
@@ -0,0 +1,72 @@
+;; c1.rkt
+#!/usr/bin/racket
+#lang racket
+
+;; ;使用define 来命名 
+;; (define pi 3.1415926)
+;; ;define 还可以定义一个名字,引用复杂的计算结果,将复杂的程序简化.
+;; (define radius 10)
+;; (define circumference (* 2 pi radius))
+
+
+;; ;定义过程 把复杂的过程定义成一个简单的函数 就是定义函数
+(define (square x) (* x x))
+;; (square 5)
+
+;条件表达式 cond
+(define (abs x)
+    (cond ((> x 0) x)
+          ((= x 0) 0)
+          ((< x 0) (- x))
+            )
+    )
+;; (abs -5)
+
+;; ;; ; 另外一个cond 版本 的abs
+;; (define (abs x)
+;;   (cond ((< x 0) (- x))
+;;         (else x))
+;;   )
+;; (abs -10)
+
+;; ;; ; other if version abs
+;; (define (abs x)
+;;   (if (< x 0)
+;;       (- x)
+;;       x
+;;       )
+;;   )
+;; (abs -4)
+
+
+;; ;average
+(define (average x y) (/ (+ x y) 2.0))
+
+
+;; ;过程内部定义过程, 这样我们只关注最主要的过程,辅助过程定义在过程内部隐藏起来
+;; ;sqrt x 开平方
+(define (sqrt x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+  (sqrt-iter 1.0 x))
+;; (sqrt 5.0)
+
+
+
+;; ;1.3 抽象出的区间sum过程  只关注于sum求和
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a) (sum term (next a) next b)))
+  )
+
+;; ;1-10 sum square
+(define (inc n) (+ n 1))
+(define (sum-square a b) (sum square a inc b))
+(sum-square 1 5)
